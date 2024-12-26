c088ea2171618126dead0f14aabbb73f47001388
biennn86 <biennn86@gmail.com>
2024-12-26 22:48:23 +0700

    rename database

--- database/conect_db.py
+++ database/conect_db.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5621862ec725980a0bb8f0b27f88257b760abdcda5d99a607ce06ad572a82ab
-size 336
+oid sha256:736eb1ecb6fc4002daa8d02b4e32fc60f7e240d8da4e0f44c0eaeb16105c5264
+size 345
