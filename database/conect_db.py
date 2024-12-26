5bef937781cb72638a4cf09d76daf73bca853aca
biennn86 <biennn86@gmail.com>
2024-12-26 23:34:40 +0700

    Update conect_db.py

--- database/conect_db.py
+++ database/conect_db.py
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fe6b3b3c2efd4c2befee9a3f5221e79846c1a5cf3f6b91ca8844c72feeadc430
-size 302
+import sqlite3
+
+class ConnectDB:
+    __NAME_DB = 'database\pg.db'
+
+    def getConection(self):
+        try:
+            self.conn = sqlite3.connect(self.__NAME_DB)
+            print("kết nối db thành công")
+            return self.conn
+        except:
+            print('Connect Database Fail.')
