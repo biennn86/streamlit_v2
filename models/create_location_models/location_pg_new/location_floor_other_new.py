159739be8451cf40810bdc139d12fe1f06de6678
biennn86 <biennn86@gmail.com>
2026-05-09 22:59:30 +0700

    fix location LTA

--- models/create_location_models/location_pg_new/location_floor_other_new.py
+++ models/create_location_models/location_pg_new/location_floor_other_new.py
@@ -3,20 +3,20 @@ from models.create_location_models.model_location_pg import KeyLoc
 
 def list_config_floor_other_new() -> list:
 	configs_floor_other_new = [
-		#ST CẤP HÀNG PM CHO XƯỞNG
+		#ST XƯỞNG RA HÀNG FG
 		FloorConfig_New(
 			location_name = ['LTA' + loc for loc in 'DX,DC,DM,DJ,AU,DK,DQ,DF,AE,AK,PD,AM,DD,DW,DZ,DV,FE,DN,JA,FX,FC,FD,FR,CA,AT,AA'.split(',')],
-			location_system_type = KeyLoc.LocSystemType.LSLPM,
+			location_system_type = KeyLoc.LocSystemType.IN,
 			rack_system_type = KeyLoc.RackSystemType.FL,
 			location_storage_type = KeyLoc.LocStorageType.FLOOR,
-			zone = KeyLoc.Zone.LSL_PM,
+			zone = KeyLoc.Zone.LSL_IN,
 			location_category = KeyLoc.LocCategory.LSL,
 			location_product_category = KeyLoc.LocProducCategory.FG_RPM,
 			name_warehouse = KeyLoc.NameWarehouse.LSL,
 			pallet_capacity = 1,
 			stack_limit = 1,
-			is_active = [],
-			status_location = KeyLoc.Status_Location.OK,
+			is_active = 0,
+			status_location = KeyLoc.Status_Location.LOCK,
 			note = None
 			),
 	]
